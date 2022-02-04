2d7191ebbfefcb363ddc1051285178b52897045a
Badma <badma.chedyrov@yahoo.com>
2022-02-04 14:07:06 +0300

    1st

--- src/Button.java
+++ src/Button.java
@@ -1,2 +1,7 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class Button {
+
+
 }
---
+++ src/Testing.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class Testing {
+    @BeforeTest{
+
+
+
+    }
+
+    @AfterTest{
+
+    }
+}
